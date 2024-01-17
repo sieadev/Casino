61fa5c2aa4b6a53ef4e246880f50566c61924a60
SIEA <69807609+DrVosss@users.noreply.github.com>
2024-01-17 16:45:04 +0100

    Initial commit

---
+++ src/main/java/dev/siea/casino/Casino.java
@@ -0,0 +1,17 @@
+package dev.siea.casino;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Casino extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
